7c645bf69f56e712c1438808f944e85d668e80c1
k-h0shikawa <81114182+k-h0shikawa@users.noreply.github.com>
2023-02-05 17:31:24 +0900

    Version1.0.0 (#4)
    * add foods component
    
    * API連携を行えるようにしました
    
    * 材料を表示できる機能を追加
    
    * 入力フォームの追加＋README作成
    
    * 保持している食材の単一入力を双方向バインディングできる機能を追加しました
    
    * 複数の入力フォームを作成
    
    * 複数の入力フォームを修正
    
    * ダミーでapiとの紐付け対応
    
    * READMEを追加
    
    * READMEを追加
    
    * API経由でメニューを取得する処理を実装 (#3)
    
    * 保持している食材の単一入力を双方向バインディングできる機能を追加しました
    
    * 複数の入力フォームを作成
    
    * 複数の入力フォームを修正
    
    * ダミーでapiとの紐付け対応
    
    * READMEを追加
    
    * READMEを追加
    
    * postでAPIを叩く処理を追加し、API経由でレシピを参照できるように変更

--- project/WhatDoIEat/src/app/api-call.service.ts
+++ project/WhatDoIEat/src/app/api-call.service.ts
@@ -8,16 +8,42 @@ import { Recipe } from './recipt';
   providedIn: 'root'
 })
 export class ApiCallService {
+
+  // テスト用ダミーデータ
+  recipes: Recipe[] = [{
+    id: 1,
+    menuName: "dummyMenu",
+    ingredients: [{id: 11, ingredientName: "dummyIngredients"}]
+  }]
   
-  apiUrl : string = 'http://localhost:8080/menu'
+  apiUrl : string = 'http://localhost:8080'
+
+
+  HTTP_OPTIONS = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json; charset=UTF-8'
+    })
+  };
+
 
   constructor( private httpclient: HttpClient) { }
 
-  /** レシピの内容を取得する関数
+  /** レシピの内容を取得する
    * 
    * */ 
   getRecipts(): Observable<Recipe[]>{
-    return this.httpclient.get<Recipe[]>(this.apiUrl);
+    return this.httpclient.get<Recipe[]>(this.apiUrl + "/menu");
+
   }
+
+  /** 入力された食材から作成できるレシピを返す */
+  getCanMakeRecipts(ownIngredients:string) : Observable<Recipe[]>{
+
+    return this.httpclient.post<any>(
+      this.apiUrl + "/canMakeMenu" , 
+      ownIngredients,
+      this.HTTP_OPTIONS
+      ) 
+    }
 
 }
--- project/WhatDoIEat/src/app/app.module.ts
+++ project/WhatDoIEat/src/app/app.module.ts
@@ -2,17 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { OwnIngredientsComponent } from './own-ingredients/own-ingredients.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent
+
+    RecipesComponent,
+    OwnIngredientsComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
